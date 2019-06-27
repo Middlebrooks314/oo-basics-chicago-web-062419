e46c8701483f90e13600a0dffe65ccefa0452a88
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-27 02:47:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
 class Book
-  attri_accessor 
+  attri_accessor :author, :page_counter, :genre 
+  
+  def initialize(title)
+    @title = title
+  end 
+  
+  def title
+    @title
+  end 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+  
+end 
