4b3b5ac324f26c886fba6c4fdac3383852a2ee83
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-27 02:54:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 
 class shoe
-  attr_accessor :brand, :color, :size,:material,:condition
+  attr_accessor :color, :size,:material,:condition
   
   attr_reader :brand 
   
